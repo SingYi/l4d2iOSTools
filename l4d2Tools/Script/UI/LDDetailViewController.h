5284ec423b51c8b4cc5cb59ec0ca36b81826e439
shi yi <shiyi@bantus.cn>
2022-03-09 17:09:15 +0800

    fix bug

---
+++ l4d2Tools/Script/UI/LDDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  LDDetailViewController.h
+//  l4d2Tools
+//
+//  Created by Sans on 2022/3/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LDDetailViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ l4d2Tools/Script/UI/LDDetailViewController.m
@@ -0,0 +1,12 @@
+//
+//  LDDetailViewController.m
+//  l4d2Tools
+//
+//  Created by Sans on 2022/3/9.
+//
+
+#import "LDDetailViewController.h"
+
+@implementation LDDetailViewController
+
+@end
